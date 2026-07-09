6e65aed206882dd3b429e90f56272b006a8683d2
mihneanob <mihneanob@gmail.com>
2026-07-09 13:20:44 +0300

    chore: untrack maintainer notes and test fixture, update license name
    Stop tracking AGENTS.md and test-playground/ (kept locally, added to
    .gitignore) to keep the public tree focused on the distributable
    product. Update LICENSE copyright to mihneaptu.

--- test-playground/src/beta.js
+++
@@ -1,9 +0,0 @@
-function beta(value) {
-  const label = "beta";
-  if (value === 0) {
-    return label;
-  }
-  return label + value;
-}
-
-console.log(beta(0));
--- test-playground/src/gamma.js
+++
@@ -1,6 +0,0 @@
-function gamma(name) {
-  const prefix = "g-";
-  return prefix + name;
-}
-
-console.log(gamma("x"));
